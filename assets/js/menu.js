7d99c1d21ee8b6d9366681e97a090d88b55be288
swati612 <surbhigarg30000@gmail.com>
2020-12-28 13:53:46 +0530

    remove extra css

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,3 +4,22 @@ $(".clickable").click(function () {
 $(".first").click(function () {
     $(this).find("ul").slideToggle(400);
 });
+
+
+      function openPage(pageName, elmnt, color, backgroundcolor) {
+         var i, tabcontent, tablinks;
+         tabcontent = document.getElementsByClassName("tabcontent");
+         for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+         }
+         tablinks = document.getElementsByClassName("tablink");
+         for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].style.color = "";
+            tablinks[i].style.backgroundColor = "";
+         }
+         document.getElementById(pageName).style.display = "block";
+         elmnt.style.color = color;
+      }
+
+      // Get the element with id="defaultOpen" and click on it
+      document.getElementById("defaultOpen").click();
